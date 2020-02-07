b6f1d37f2d1a0c820810c3f8f86330701d7536b3
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2020-02-07 17:42:03 +0530

    Update script.js

--- To-Do-Project-1/script.js
+++ To-Do-Project-1/script.js
@@ -1,7 +1,41 @@
 // get the app items
+var theList = document.getElementById('theList'),
+    form = document.querySelector('form'),
+    input = form.querySelector('input'),
+    cache = {};
 
+// on submit
+form.addEventListener('submit', function (ev) {
+    // "use strict"; //ECMAScript version 5 compliant
+    ev.preventDefault();
 
-// on Submit
+    // add item to the list
+    var value = input.value;
 
+    if (!cache[value] && !(value == "")) {
+        // cache the users input
+        cache[value] = true;
+        theList.innerHTML += '<li>' + value + '</li>';
 
-//load local storage
+        //store using localstorage
+        localStorage.myToDo = theList.innerHTML;
+    }
+});
+
+theList.addEventListener('click', function (ev) {
+    //"use strict";
+    var t = ev.target;
+
+    if (!t.classList.contains('done')) {
+        t.classList.add('done');
+    } else {
+        t.parentNode.removeChild(t);
+    }
+    localStorage.myToDo = theList.innerHTML;
+});
+
+//load local storage on first load
+if (localStorage.myToDo !== undefined) {
+    theList.innerHTML = localStorage.myToDo;
+    console.log(localStorage)
+}
